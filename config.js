3793871c0a800c807d749e17b78ee1fdaf586f7d
ikramChourib <32400696+ikramChourib@users.noreply.github.com>
2026-09-22 13:17:58 +0200

    Update Google Apps Script URL in config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // maintenant depuis admin.html, une fois ce lien renseigné.
 const QUIZ_CONFIG = {
   // URL du Google Apps Script (Web App). Voir README.md.
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycbxuAW0g4oCxptXUfUiX0J7ap7-lsAc9hN22eW31jprhdUjWA4YMfza_J1k1xMPkJa_Q/exec"
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbzQ1PEEGQNJBwQeh32Ehxo2B_yWHKF2Y-SDc94rKI5h-16KR-xr1qgW3u-jz03LhqRk/exec"
 };
